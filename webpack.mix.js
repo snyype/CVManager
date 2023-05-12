d5efa32218fe7885b5c27507cb06b9633cb0c997
Aashish Sharma <snypeatwork@gmail.com>
2023-05-12 13:32:22 +0545

    setpublicpath of dist

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css',[
-    require('tailwindcss'),
-]);
+mix.setPublicPath('dist')
+   .js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css', [
+       require('tailwindcss')
+   ]);
