bacd964f3d8ef32b8623a8aa9760ee8d528e5e53
Aashish Sharma <snypeatwork@gmail.com>
2023-05-12 12:39:17 +0545

    Changes To package.json

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
